e93508b5a9819fbe8fd168e08546b987c9ae9d3d
Jacques Marneweck <jacques@siberia.co.za>
2021-03-06 22:46:24 +0200

    Add constants for colours per Excel default colours

---
+++ src/Style/Colour.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018-2021 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+namespace Jacques\Reports\Style;
+
+final class Colour
+{
+    const COLOUR_BLACK = 'FF000000';
+    const COLOUR_BROWN = 'FF993300';
+    const COLOUR_OLIVE_GREEN = 'FF333300';
+    const COLOUR_DARK_GREEN = 'FF003300';
+    const COLOUR_DARK_TEAL = 'FF003366';
+    const COLOUR_DARK_BLUE = 'FF000080';
+    const COLOUR_INDIGO = 'FF333399';
+    const COLOUR_GREY_80 = 'FF333333';
+    const COLOUR_DARK_RED = 'FF800000';
+    const COLOUR_ORANGE = 'FFFF6600';
+    const COLOUR_DARK_YELLOW = 'FF808000';
+    const COLOUR_GREEN = 'FF008000';
+    const COLOUR_TEAL = 'FF008080';
+    const COLOUR_BLUE = 'FF0000FF';
+    const COLOUR_BLUE_GRAY = 'FF666699';
+    const COLOUR_GRAY_50 = 'FF808080';
+    const COLOUR_RED = 'FFFF0000';
+    const COLOUR_LIGHT_ORANGE = 'FFFF9900';
+    const COLOUR_LIME = 'FF99CC00';
+    const COLOUR_SEA_GREEN = 'FF339966';
+    const COLOUR_AQUA = 'FF33CCCC';
+    const COLOUR_LIGHT_BLUE = 'FF3366FF';
+    const COLOUR_VIOLET = 'FF800080';
+    const COLOUR_GRAY_40 = 'FF969696';
+    const COLOUR_PINK = 'FFFF00FF';
+    const COLOUR_GOLD = 'FFFFCC00';
+    const COLOUR_YELLOW = 'FFFFFF00';
+    const COLOUR_BRIGHT_GREEN = 'FF00FF00';
+    const COLOUR_TURQUOISE = 'FF00FFFF';
+    const COLOUR_SKY_BLUE = 'FF00CCFF';
+    const COLOUR_PLUM = 'FF993366';
+    const COLOUR_GRAY_25 = 'FFC0C0C0';
+    const COLOUR_ROSE = 'FFFF99CC';
+    const COLOUR_TAN = 'FFFFCC99';
+    const COLOUR_LIGHT_YELLOW = 'FFFFFF99';
+    const COLOUR_LIGHT_GREEN = 'FFCCFFCC';
+    const COLOUR_LIGHT_TURQUOISE = 'FFCCFFFF';
+    const COLOUR_PALE_BLUE = 'FF99CCFF';
+    const COLOUR_LAVENDAR = 'FFCC99FF';
+    const COLOUR_WHITE = 'FFFFFFFF';
+}
