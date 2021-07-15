efd831f74ae700b7497421960f2ccf271987eeb6
Jacques Marneweck <jacques@siberia.co.za>
2021-07-15 17:58:29 +0200

    Update rector config

--- rector.php
+++ rector.php
@@ -5,29 +5,35 @@
  */
 
 use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
+    $services = $containerConfigurator->services();
+
     $parameters->set(Option::PATHS, ['src', 'tests']);
 
-    $parameters->set(Option::SETS, [
+    foreach([
         SetList::CODE_QUALITY,
         SetList::CODE_QUALITY_STRICT,
         SetList::CODING_STYLE,
-        SetList::PERFORMANCE,
-        SetList::PHPUNIT_60,
-        SetList::PHPUNIT_70,
-        SetList::PHPUNIT_75,
-        SetList::PHPUNIT_80,
-        SetList::PHPUNIT_90,
-        SetList::PHPUNIT_91,
+        #SetList::PERFORMANCE,
+        PHPUnitSetList::PHPUNIT_60,
+        PHPUnitSetList::PHPUNIT_70,
+        PHPUnitSetList::PHPUNIT_75,
+        PHPUnitSetList::PHPUNIT_80,
+        PHPUnitSetList::PHPUNIT_90,
+        PHPUnitSetList::PHPUNIT_91,
         SetList::PHP_70,
         SetList::PHP_71,
         SetList::PHP_72,
         SetList::PHP_73,
         SetList::PHP_74,
-    ]);
+    ] as $set) {
+        $containerConfigurator->import($set);
+    }
 };
