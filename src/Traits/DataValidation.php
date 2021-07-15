4fa2bfc8f8adb33a693e874b32dd85ce36ee229b
Jacques Marneweck <jacques@siberia.co.za>
2021-07-15 18:03:31 +0200

    Don't quote '\'s

---
+++ src/Traits/DataValidation.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018-2021 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+namespace Jacques\Reports\Traits;
+
+trait DataValidation
+{
+    /**
+     * Draws borders.
+     *
+     * @param \Jacques\Report\Excel $report
+     * @param string $coords
+     *
+     * @void
+     */
+    public function dropdown(\Jacques\Reports\Excel $report, string $coords, ?string $type = 'outer'): void
+    {
+
+    }
+}
