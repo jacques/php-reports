067a6186e9f0ce1b0ab060ae1598fed861d5e621
Jacques Marneweck <jacques@siberia.co.za>
2021-07-15 18:09:04 +0200

    Specify const type

--- src/Style/Colour.php
+++ src/Style/Colour.php
@@ -8,44 +8,164 @@ namespace Jacques\Reports\Style;
 
 final class Colour
 {
+    /**
+     * @var string
+     */
     const COLOUR_BLACK = 'FF000000';
+    /**
+     * @var string
+     */
     const COLOUR_BROWN = 'FF993300';
+    /**
+     * @var string
+     */
     const COLOUR_OLIVE_GREEN = 'FF333300';
+    /**
+     * @var string
+     */
     const COLOUR_DARK_GREEN = 'FF003300';
+    /**
+     * @var string
+     */
     const COLOUR_DARK_TEAL = 'FF003366';
+    /**
+     * @var string
+     */
     const COLOUR_DARK_BLUE = 'FF000080';
+    /**
+     * @var string
+     */
     const COLOUR_INDIGO = 'FF333399';
+    /**
+     * @var string
+     */
     const COLOUR_GREY_80 = 'FF333333';
+    /**
+     * @var string
+     */
     const COLOUR_DARK_RED = 'FF800000';
+    /**
+     * @var string
+     */
     const COLOUR_ORANGE = 'FFFF6600';
+    /**
+     * @var string
+     */
     const COLOUR_DARK_YELLOW = 'FF808000';
+    /**
+     * @var string
+     */
     const COLOUR_GREEN = 'FF008000';
+    /**
+     * @var string
+     */
     const COLOUR_TEAL = 'FF008080';
+    /**
+     * @var string
+     */
     const COLOUR_BLUE = 'FF0000FF';
+    /**
+     * @var string
+     */
     const COLOUR_BLUE_GRAY = 'FF666699';
+    /**
+     * @var string
+     */
     const COLOUR_GRAY_50 = 'FF808080';
+    /**
+     * @var string
+     */
     const COLOUR_RED = 'FFFF0000';
+    /**
+     * @var string
+     */
     const COLOUR_LIGHT_ORANGE = 'FFFF9900';
+    /**
+     * @var string
+     */
     const COLOUR_LIME = 'FF99CC00';
+    /**
+     * @var string
+     */
     const COLOUR_SEA_GREEN = 'FF339966';
+    /**
+     * @var string
+     */
     const COLOUR_AQUA = 'FF33CCCC';
+    /**
+     * @var string
+     */
     const COLOUR_LIGHT_BLUE = 'FF3366FF';
+    /**
+     * @var string
+     */
     const COLOUR_VIOLET = 'FF800080';
+    /**
+     * @var string
+     */
     const COLOUR_GRAY_40 = 'FF969696';
+    /**
+     * @var string
+     */
     const COLOUR_PINK = 'FFFF00FF';
+    /**
+     * @var string
+     */
     const COLOUR_GOLD = 'FFFFCC00';
+    /**
+     * @var string
+     */
     const COLOUR_YELLOW = 'FFFFFF00';
+    /**
+     * @var string
+     */
     const COLOUR_BRIGHT_GREEN = 'FF00FF00';
+    /**
+     * @var string
+     */
     const COLOUR_TURQUOISE = 'FF00FFFF';
+    /**
+     * @var string
+     */
     const COLOUR_SKY_BLUE = 'FF00CCFF';
+    /**
+     * @var string
+     */
     const COLOUR_PLUM = 'FF993366';
+    /**
+     * @var string
+     */
     const COLOUR_GRAY_25 = 'FFC0C0C0';
+    /**
+     * @var string
+     */
     const COLOUR_ROSE = 'FFFF99CC';
+    /**
+     * @var string
+     */
     const COLOUR_TAN = 'FFFFCC99';
+    /**
+     * @var string
+     */
     const COLOUR_LIGHT_YELLOW = 'FFFFFF99';
+    /**
+     * @var string
+     */
     const COLOUR_LIGHT_GREEN = 'FFCCFFCC';
+    /**
+     * @var string
+     */
     const COLOUR_LIGHT_TURQUOISE = 'FFCCFFFF';
+    /**
+     * @var string
+     */
     const COLOUR_PALE_BLUE = 'FF99CCFF';
+    /**
+     * @var string
+     */
     const COLOUR_LAVENDAR = 'FFCC99FF';
+    /**
+     * @var string
+     */
     const COLOUR_WHITE = 'FFFFFFFF';
 }
