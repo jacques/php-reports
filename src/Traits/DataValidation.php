31ebb09966a71654feabb4938bb5c347503fe2a2
Jacques Marneweck <jacques@siberia.co.za>
2025-10-22 07:12:08 +0000

    Add trait for drop-down list building

--- src/Traits/DataValidation.php
+++ src/Traits/DataValidation.php
@@ -9,15 +9,34 @@ namespace Jacques\Reports\Traits;
 trait DataValidation
 {
     /**
-     * Draws borders.
+     * Drop-down list for data validation.
      *
      * @param \Jacques\Report\Excel $report
      * @param string $coords
+     * @param array|string the options for the drop-down list.
+     * @param string $title
+     * @param string $prompt
      *
      * @void
      */
-    public function dropdown(\Jacques\Reports\Excel $report, string $coords, ?string $type = 'outer'): void
+    public function dropdown(\Jacques\Reports\Excel $report, string $coords, array|string $options, string $title = 'Pick from list', string $prompt = 'Please pick a value from the drop-down list.'): void
     {
+        $validation = $report->getActiveSheet()->getCell($cell)->getDataValidation();
+        $validation->setType(DataValidation::TYPE_LIST)
+            ->setErrorStyle(DataValidation::STYLE_INFORMATION)
+            ->setAllowBlank(false)
+            ->setShowInputMessage(true)
+            ->setShowErrorMessage(true)
+            ->setShowDropDown(true)
+            ->setErrorTitle('Input error')
+            ->setError('Value is not in list.')
+            ->setPromptTitle($title)
+            ->setPrompt($prompt);
 
+        if (is_array($options)) {
+            $validation->setFormula1('"' . implode(',', $options) . '"');
+        } else {
+            $validation->setFormula1($options);
+        }
     }
 }
